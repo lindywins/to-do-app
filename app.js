578bdafb7e487f122bf3e250b928133fe63cf667
M <melindawest14@gmail.com>
2019-03-18 22:01:48 -0600

    added js and css

---
+++ app.js
@@ -0,0 +1,51 @@
+function onReady() {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.addEventListener('submit',
+    function(event) {
+      event.preventDefault();
+
+      // get the text
+      let title = newToDoText.value;
+
+      // create a new li
+      let newLi = document.createElement('li');
+
+      // create a new input
+      let checkbox = document.createElement('input');
+
+      // create delete button
+      let deleteBtn = document.createElement('button');
+
+      // set the input's type to checkbox
+      checkbox.type = "checkbox";
+
+      // set the title
+      newLi.textContent = title;
+
+      // attach the checkbox to the li
+      newLi.appendChild(checkbox);
+
+      // attach delete button
+      newLi.appendChild(deleteBtn);
+      deleteBtn.textContent = "Delete";
+
+      // attach the li to the ul
+      toDoList.appendChild(newLi);
+
+      // empty the input
+      newToDoText.value = '';
+
+      // delete li
+      deleteBtn.addEventListener('click',
+        function (event) {
+          newLi.remove();
+        });
+
+})}
+
+window.onload = function() {
+  onReady();
+};
